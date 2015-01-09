23d7fab6f7d0778ef7768bc940a0079d7f546d11
andela-ladenusi <ladi.adenusi@andela.co>
2015-01-09 14:42:22 +0100

    CORS

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get('/', function(request, response) {
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With');
   next();
 };
 
